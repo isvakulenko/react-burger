0bb50e0cc4d49076016ab38ebd9ba3993c58f3b2
isvakulenko <ivan-vakulenko@yandex.ru>
2022-05-27 23:30:17 +0300

    Add POST request

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,16 +1,31 @@
 //Адрес. с которого вытягиваем данные об ингридиетах
-const Url = "https://norma.nomoreparties.space/api/ingredients";
+const Url = "https://norma.nomoreparties.space/api";
 
 function checkResponse(res) {
   return res.ok ? res.json() : Promise.reject(`Ошибка: ${res.status}`);
 }
-
-export const getIngredients = () => {
-  return fetch(Url)
-    .then(checkResponse)
-    // .then(res => {console.log (res)})
-    .then((data) => {
-      if (data.success) return data.data;
-      return Promise.reject(data);
-    });
+// Получаем данные с сервера с разными ингредиентами
+const getIngredients = () => {
+  return (
+    fetch(`${Url}/ingredients`)
+      .then(checkResponse)
+      // .then(res => {console.log (res)})
+      .then((data) => {
+        if (data.success) return data.data;
+        return Promise.reject(data);
+      })
+  );
 };
+// Отправляем данные на сервер.
+//Данные в виде массива с id ингредиентов.
+//В ответ сервер сформирует номер заказа.
+const saveOrder = (idIngredientsArr) => {
+  return fetch(`${Url}/orders`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json;charset=utf-8",
+    },
+    body: JSON.stringify({ ingredients: idIngredientsArr }),
+  }).then(checkResponse);
+};
+export { getIngredients, saveOrder };
