5a6ced00d81a1000e804deb1cd2c2532c5db05ac
isvakulenko <ivan-vakulenko@yandex.ru>
2022-05-28 12:30:15 +0300

    Transfer state to Context

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,14 +1,68 @@
-import React from "react";
+import { useState, useEffect, useReducer } from "react";
 import styles from "./app.module.css";
 import AppHeader from "../app-header/app-header";
 import BurgerIngredients from "../burger-ingredients/burger-ingredients";
 import BurgerConstructor from "../burger-constructor/burger-constructor";
 import { getIngredients } from "../../utils/api";
+import { BurgerIngredientsContext } from "../../contex/burger-ingredients-context";
+import { BurgerConstructorContext } from "../../contex/burger-constructor-context";
+
+// Cтейт текущего бургер-конструктора,
+//т.е. всё то, что лежит внутри него, тем и наполнен бургер.
+const constructorInitialState = {
+  bun: null,
+  ingredients: [],
+  order: [],
+};
+// Функция reducer отвечает за добавление, удаление ингредиентов
+function constructorReducer(state, action) {
+  switch (action.type) {
+    case "ADD":
+      if (action.payload.type === "bun") {
+        return {
+          ...state,
+          bun: action.payload,
+          order: [...state.order, action.payload._id],
+        };
+      }
+      return {
+        ...state,
+        ingredients: [...state.ingredients, action.payload],
+        order: [...state.order, action.payload._id],
+      };
+
+    case "DELETE":
+      if (action.payload.type === "bun") {
+        return {
+          ...state,
+          bun: null,
+          order: state.order.filter((id) => id !== action.payload._id),
+        };
+      }
+      return {
+        ...state,
+        ingredients: state.ingredients.filter(
+          (item) => item._id !== action.payload._id
+        ),
+        order: state.order.filter((id) => id !== action.payload._id),
+      };
+
+    case "RESET":
+      return constructorInitialState;
+
+    default:
+      return state;
+  }
+}
 
 function App() {
-  const [ingredients, setIngredients] = React.useState([]);
-
-  React.useEffect(() => {
+  const [ingredients, setIngredients] = useState([]);
+  const [constructorState, constructorDispatcher] = useReducer(
+    constructorReducer,
+    constructorInitialState
+  );
+//Запросим ингредиенты с сервера
+  useEffect(() => {
     getIngredients()
       .then((res) => {
         setIngredients(res);
@@ -18,14 +72,26 @@ function App() {
       });
   }, []);
   // console.log(ingredients);
-
+  // console.log(constructorState);
   return (
     <div className={`${styles.main} pt-10 pb-10`}>
       <AppHeader />
-      <div className={styles.wrap}>
-        <BurgerIngredients ingredients={ingredients} />
-        <BurgerConstructor ingredientsConstructor={ingredients} />
-      </div>
+      {constructorState && (
+        <div className={styles.wrap}>
+          <BurgerConstructorContext.Provider
+            value={{ constructorState, constructorDispatcher }}
+          >
+            <BurgerIngredientsContext.Provider value={ingredients}>
+              <BurgerIngredients />
+            </BurgerIngredientsContext.Provider>
+          </BurgerConstructorContext.Provider>
+          <BurgerConstructorContext.Provider
+            value={{ constructorState, constructorDispatcher }}
+          >
+            <BurgerConstructor />
+          </BurgerConstructorContext.Provider>
+        </div>
+      )}
     </div>
   );
 }
--- src/components/burger-constructor/burger-constructor.jsx
+++ src/components/burger-constructor/burger-constructor.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useMemo, useState, useContext } from "react";
 import {
   Button,
   DragIcon,
@@ -6,36 +6,67 @@ import {
   CurrencyIcon,
 } from "@ya.praktikum/react-developer-burger-ui-components";
 import styles from "./burger-constructor.module.css";
-import PropTypes from "prop-types";
-import { IngredientPropTypes } from "../../utils/prop-types";
+
 import Modal from "../modal/modal";
 import OrderDetails from "../order-details/order-details";
 
-const BurgerConstructor = ({ ingredientsConstructor }) => {
+import { BurgerConstructorContext } from "../../contex/burger-constructor-context";
+import { saveOrder } from "../../utils/api";
+
+const BurgerConstructor = () => {
+  const { constructorState, constructorDispatcher } = useContext(
+    BurgerConstructorContext
+  );
+  const [modalData, setModalData] = useState(null);
   // Булевый стейт для модалки заказа
-  const [isOrderDetailsOpened, setIsOrderDetailsOpened] = React.useState(false);
+  const [isOrderDetailsOpened, setIsOrderDetailsOpened] = useState(null);
   const openOrderDetail = () => setIsOrderDetailsOpened(true);
   const closeOrderDetail = () => setIsOrderDetailsOpened(false);
 
-  const otherIngredients = ingredientsConstructor.filter(
-    (item) => item.type !== "bun"
-  );
+  //Подсчитаем итоговую стоимость заказа
 
-  console.log(otherIngredients);
+  const price = useMemo(() => {
+    return (
+      (constructorState.bun ? constructorState.bun.price * 2 : 0) +
+      constructorState.ingredients.reduce(
+        (sum, current) => sum + current.price,
+        0
+      )
+    );
+  }, [constructorState]);
+
+  // открываем модальное окно
+  const handleOrderClick = () => {
+    // сохраняем ингредиенты на сервер
+    saveOrder(constructorState.order).then((data) => {
+      // полученный ответ помещаем в стейт для модалки
+      setModalData(data);
+      openOrderDetail();
+      // в ответе номер заказа лежит в data.order.number
+    });
+  };
+
   return (
     <section className={`${styles.menu} pt-25`}>
-      <div className="mt-4 mb-4">
-        <ConstructorElement
-          type="top"
-          isLocked={true}
-          text="Краторная булка N-200i (верх)"
-          price={200}
-          thumbnail={"https://code.s3.yandex.net/react/code/bun-02-mobile.png"}
-        />
-      </div>
+      {constructorState.bun && (
+        <div className="mt-4 mb-4">
+          <ConstructorElement
+            type="top"
+            text={`${constructorState.bun.name} (верх)`}
+            price={constructorState.bun.price}
+            thumbnail={constructorState.bun.image}
+            handleClose={() =>
+              constructorDispatcher({
+                type: "DELETE",
+                payload: constructorState.bun,
+              })
+            }
+          />
+        </div>
+      )}
       <div className={styles.content}>
         <ul className={styles.list}>
-          {otherIngredients.map((item, index) => {
+          {constructorState.ingredients.map((item, index) => {
             return (
               <li className={`${styles.item} mt-4 mb-4`} key={index}>
                 <DragIcon />
@@ -44,42 +75,42 @@ const BurgerConstructor = ({ ingredientsConstructor }) => {
                   text={item.name}
                   price={item.price}
                   thumbnail={item.image}
+                  handleClose={() =>
+                    constructorDispatcher({ type: "DELETE", payload: item })
+                  }
                 />
               </li>
             );
           })}
         </ul>
       </div>
-      <div className="mt-4 mb-4">
-        <ConstructorElement
-          type="bottom"
-          isLocked={true}
-          text="Краторная булка N-200i (верх)"
-          price={200}
-          thumbnail={"https://code.s3.yandex.net/react/code/bun-02-mobile.png"}
-        />
-      </div>
+      {constructorState.bun && (
+        <div className="mt-4 mb-4">
+          <ConstructorElement
+            type="bottom"
+            isLocked={true}
+            text={`${constructorState.bun.name} (низ)`}
+            price={constructorState.bun.price}
+            thumbnail={constructorState.bun.image}
+          />
+        </div>
+      )}
       <div className={styles.order}>
         <div className={`${styles.cost} mr-10`}>
-          <p className="text text_type_digits-medium mr-2">610</p>
+          <p className="text text_type_digits-medium mr-2">{price}</p>
           <CurrencyIcon type="primary" />
         </div>
-        <Button type="primary" size="large" onClick={openOrderDetail}>
+        <Button type="primary" size="large" onClick={handleOrderClick}>
           Оформить заказ
         </Button>
       </div>
       {isOrderDetailsOpened && (
         <Modal onClose={closeOrderDetail}>
-          <OrderDetails orderNumber={1234567} />
+          <OrderDetails orderNumber={modalData.order.number} />
         </Modal>
       )}
     </section>
   );
-};
-
-BurgerConstructor.propTypes = {
-  ingredientsConstructor: PropTypes.arrayOf(IngredientPropTypes.isRequired)
-    .isRequired,
 };
 
 export default BurgerConstructor;
