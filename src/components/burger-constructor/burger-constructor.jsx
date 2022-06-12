1c8eca54c4dd88f1702c9c43bacb47513901168d
isvakulenko <ivan-vakulenko@yandex.ru>
2022-06-12 16:36:19 +0300

    refacto for Redux, add DND

--- src/components/burger-constructor/burger-constructor.jsx
+++ src/components/burger-constructor/burger-constructor.jsx
@@ -1,27 +1,29 @@
-import React, { useMemo, useState, useContext } from "react";
+import { useMemo } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useDrop } from "react-dnd";
 import {
   Button,
-  DragIcon,
   ConstructorElement,
   CurrencyIcon,
 } from "@ya.praktikum/react-developer-burger-ui-components";
 import styles from "./burger-constructor.module.css";
-
 import Modal from "../modal/modal";
+import BurgerConstructorComponent from "../burger-constructor-component/burger-constructor-component";
 import OrderDetails from "../order-details/order-details";
-
-import { BurgerConstructorContext } from "../../contex/burger-constructor-context";
-import { saveOrder } from "../../utils/api";
+import { addToConstructor } from "../../services/actions/constructor";
+import { orderBurger, RESET_ORDER } from "../../services/actions/order";
 
 const BurgerConstructor = () => {
-  const { constructorState, constructorDispatcher } = useContext(
-    BurgerConstructorContext
-  );
-  const [modalData, setModalData] = useState(null);
-  // Булевый стейт для модалки заказа
-  const [isOrderDetailsOpened, setIsOrderDetailsOpened] = useState(null);
-  const openOrderDetail = () => setIsOrderDetailsOpened(true);
-  const closeOrderDetail = () => setIsOrderDetailsOpened(false);
+  const dispatch = useDispatch();
+  const constructorState = useSelector((state) => state.burgerConstructor);
+
+  const orderModalData = useSelector((state) => state.order.data);
+  const isOrderRequest = useSelector((state) => state.order.isloading);
+
+  const [, drop] = useDrop({
+    accept: "add_ingredient",
+    drop: (item) => dispatch(addToConstructor(item)),
+  });
 
   //Подсчитаем итоговую стоимость заказа
 
@@ -35,51 +37,43 @@ const BurgerConstructor = () => {
     );
   }, [constructorState]);
 
-  // открываем модальное окно
   const handleOrderClick = () => {
-    // сохраняем ингредиенты на сервер
-    saveOrder(constructorState.order).then((data) => {
-      // полученный ответ помещаем в стейт для модалки
-      setModalData(data);
-      openOrderDetail();
-      // в ответе номер заказа лежит в data.order.number
-    });
+    // без булок не отправляем запрос
+    if (!constructorState.bun || isOrderRequest) return;
+    // отправляем ингредиенты на сервер
+    dispatch(
+      orderBurger(
+        [constructorState.bun._id]
+          .concat(constructorState.ingredients.map((ing) => ing._id))
+          .concat(constructorState.bun._id)
+      )
+    );
   };
+  const closeOrderDetail = () => dispatch({ type: RESET_ORDER });
 
   return (
-    <section className={`${styles.menu} pt-25`}>
+    <section className={`${styles.menu} pt-25`} ref={drop}>
       {constructorState.bun && (
         <div className="mt-4 mb-4">
           <ConstructorElement
             type="top"
+            isLocked={true}
             text={`${constructorState.bun.name} (верх)`}
             price={constructorState.bun.price}
             thumbnail={constructorState.bun.image}
-            handleClose={() =>
-              constructorDispatcher({
-                type: "DELETE",
-                payload: constructorState.bun,
-              })
-            }
           />
         </div>
       )}
+
       <div className={styles.content}>
         <ul className={styles.list}>
           {constructorState.ingredients.map((item, index) => {
             return (
-              <li className={`${styles.item} mt-4 mb-4`} key={index}>
-                <DragIcon />
-                <ConstructorElement
-                  isLocked={false}
-                  text={item.name}
-                  price={item.price}
-                  thumbnail={item.image}
-                  handleClose={() =>
-                    constructorDispatcher({ type: "DELETE", payload: item })
-                  }
-                />
-              </li>
+              <BurgerConstructorComponent
+                ingredient={item}
+                index={index}
+                key={item.id}
+              />
             );
           })}
         </ul>
@@ -104,9 +98,9 @@ const BurgerConstructor = () => {
           Оформить заказ
         </Button>
       </div>
-      {isOrderDetailsOpened && (
+      {orderModalData && (
         <Modal onClose={closeOrderDetail}>
-          <OrderDetails orderNumber={modalData.order.number} />
+          <OrderDetails orderNumber={orderModalData.order.number} />
         </Modal>
       )}
     </section>
