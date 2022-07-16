332a5577943b0d6c054b7091196855e4e89aa574
isvakulenko <ivan-vakulenko@yandex.ru>
2022-07-16 09:50:39 +0300

    Add pages and routes

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,21 +1,114 @@
-import { DndProvider } from "react-dnd";
-import { HTML5Backend } from "react-dnd-html5-backend";
-import styles from "./app.module.css";
+import { Switch, Route } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
+import {
+  LoginPage,
+  HomePage,
+  RegisterPage,
+  ForgotPasswordPage,
+  ResetPasswordPage,
+  ProfilePage,
+  NotFoundPage,
+} from "../../pages";
 import AppHeader from "../app-header/app-header";
-import BurgerIngredients from "../burger-ingredients/burger-ingredients";
-import BurgerConstructor from "../burger-constructor/burger-constructor";
+import { getCookie } from "../../utils/cookie";
+//import Loader from "../loader/loader";
+import Modal from "../modal/modal";
+import IngredientInfo from "../ingredient-info/ingredient-info";
+import { getItems } from "../../services/actions/ingredient";
+import { checkAuth } from "../../services/actions/user";
+import { ProtectedRoute } from "../proptected-route/protected-route";
 
 function App() {
+  const dispatch = useDispatch();
+  const { isAuthChecked } = useSelector((store) => store.user);
+  const ingredientInModal = useSelector((state) => state.ingredientDetail.info);
+  //console.log('isAuthChecked', isAuthChecked);
+
+  const location = useLocation();
+  //Опциональная цепочка ?. останавливает вычисление
+  // и возвращает undefined, если значение перед ?. равно undefined или null.
+  const background = location.state?.background;
+  //let background = location.state && location.state.background;
+  // const accessToken = getCookie('accessToken');
+  // console.log(`Bearer ${accessToken}`);
+  // При загрузке страницы запросим все ингредиенты с сервера
+  useEffect(() => {
+    dispatch(getItems());
+    // А также проверим, авторизован ли пользователь
+    dispatch(checkAuth());
+  }, [dispatch]);
+
+  console.log("isAuthChecked", isAuthChecked);
+
   return (
-    <div className={`${styles.main} pt-10 pb-10`}>
-      <AppHeader />
-      <div className={styles.wrap}>
-        <DndProvider backend={HTML5Backend}>
-          <BurgerIngredients />
-          <BurgerConstructor />
-        </DndProvider>
-      </div>
-    </div>
+    <>
+      {!isAuthChecked && (
+        <p className="text text_type_main-medium">Загружаем...</p>
+      )}
+      {isAuthChecked && (
+        <>
+          <AppHeader />
+          <Switch location={background || location}>
+            {/* Вход */}
+            <ProtectedRoute anonymous={true} path="/login">
+              <LoginPage />
+            </ProtectedRoute>
+            {/* Конструктор */}
+            <Route path="/" exact={true}>
+              <HomePage />
+            </Route>
+            {/* Регистрация */}
+            <Route anonymous={true} path="/register" exact={true}>
+              <RegisterPage />
+            </Route>
+            {/* Восстановление пароля */}
+            <ProtectedRoute
+              anonymous={true}
+              path="/forgot-password"
+              exact={true}
+            >
+              <ForgotPasswordPage />
+            </ProtectedRoute>
+            {/* Сброс пароля */}
+            <ProtectedRoute
+              anonymous={true}
+              path="/reset-password"
+              exact={true}
+            >
+              <ResetPasswordPage />
+            </ProtectedRoute>
+            {/* Попап с модальным окном */}
+            <Route
+              path="/ingredients/:id"
+              exact={true}
+              children={<IngredientInfo ingredientData={ingredientInModal} />}
+            />
+            {/* Страница профиля*/}
+            <ProtectedRoute path="/profile" exact={true}>
+              <ProfilePage />
+            </ProtectedRoute>
+            {/* Если адрес неверный */}
+            <Route path="*">
+              <NotFoundPage />
+            </Route>
+          </Switch>
+
+          {/* Попап с модальным окном */}
+          {background && (
+            <Route
+              path="/ingredients/:id"
+              children={
+                <Modal>
+                  <IngredientInfo ingredientData={ingredientInModal} />
+                </Modal>
+              }
+            />
+          )}
+        </>
+      )}
+    </>
   );
 }
 
