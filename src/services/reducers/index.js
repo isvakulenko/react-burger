db2591e976b8779ff986d206bc16197dee56a869
isvakulenko <ivan-vakulenko@yandex.ru>
2022-07-16 08:02:59 +0300

    Add userReducer

--- src/services/reducers/index.js
+++ src/services/reducers/index.js
@@ -1,13 +1,14 @@
-import { combineReducers } from 'redux';
-import { constructorReducer } from './constructor';
-import { ingredientDetailReducer } from './ingredient-detail';
-import { ingredientReducer } from './ingredient';
-import { orderReducer } from './order';
+import { combineReducers } from "redux";
+import { constructorReducer } from "./constructor";
+import { ingredientDetailReducer } from "./ingredient-detail";
+import { ingredientReducer } from "./ingredient";
+import { orderReducer } from "./order";
+import { userReducer } from "./user";
 
 export const rootReducer = combineReducers({
   ingredientDetail: ingredientDetailReducer,
   burgerConstructor: constructorReducer,
   ingredients: ingredientReducer,
   order: orderReducer,
+  user: userReducer,
 });
-
