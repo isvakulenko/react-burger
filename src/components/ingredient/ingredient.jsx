44122d359eeff872d4ac0728d9fe8cb3c0e3a791
Ivan Vakulenko <ivan-vakulenko@yandex.ru>
2022-07-16 22:03:04 +0300

    Fix for IngredientInfo

--- src/components/ingredient/ingredient.jsx
+++ src/components/ingredient/ingredient.jsx
@@ -3,17 +3,23 @@ import {
   CurrencyIcon,
 } from "@ya.praktikum/react-developer-burger-ui-components";
 import styles from "./ingredient.module.css";
-import PropTypes from "prop-types";
 import { IngredientPropTypes } from "../../utils/prop-types";
 import { useDrag } from "react-dnd";
-import {Link, useLocation } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { onIngredientClick } from "../../services/actions/ingredient-detail";
 
-const Ingredient = ({ ingredientData, count, onClick }) => {
+const Ingredient = ({
+  ingredientData,
+  count,
+  //  onClick
+}) => {
   const { image, price, name, _id } = ingredientData;
 
-  //При нажатии на ингредиент его данные передаются в модалку
+  const dispatch = useDispatch();
+
   const handleClickIngredient = () => {
-    onClick(ingredientData);
+    dispatch(onIngredientClick(ingredientData));
   };
 
   const [, dragRef] = useDrag({
@@ -22,16 +28,16 @@ const Ingredient = ({ ingredientData, count, onClick }) => {
   });
   const location = useLocation();
   return (
-
-
-<Link
-  to={`/ingredients/${_id}`}
-  state={{background: location}}
-  className={`${styles.card} mt-6`}
-  onClick={handleClickIngredient}
-  ref={dragRef}
-  draggable
->   
+    <Link
+      to={{
+        pathname: `/ingredients/${_id}`,
+        state: { background: location },
+      }}
+      className={`${styles.card} mt-6`}
+      onClick={handleClickIngredient}
+      ref={dragRef}
+      draggable
+    >
       {count && <Counter count={count} />}
       <img className={styles.img} src={image} alt={name} />
       <div className={`${styles.cost} mt-1 mb-1 `}>
@@ -39,13 +45,13 @@ const Ingredient = ({ ingredientData, count, onClick }) => {
         <CurrencyIcon />
       </div>
       <p className={`${styles.name} text text_type_main-default`}>{name}</p>
-      </Link>
+    </Link>
   );
 };
 
 Ingredient.propTypes = {
   ingredientData: IngredientPropTypes.isRequired,
-  onClick: PropTypes.func.isRequired,
+  // onClick: PropTypes.func.isRequired,
 };
 
 export default Ingredient;
--- src/components/ingredients-type/ingredients-type.jsx
+++ src/components/ingredients-type/ingredients-type.jsx
@@ -1,56 +1,59 @@
-import React, {useMemo}  from 'react';
+import React, { useMemo } from "react";
 import Ingredient from "../ingredient/ingredient";
-import { useSelector } from 'react-redux';
+import { useSelector } from "react-redux";
 import styles from "./ingredients-type.module.css";
 import PropTypes from "prop-types";
 import { IngredientPropTypes } from "../../utils/prop-types";
 
-const IngredientsType = React.forwardRef(({
-  title,
-  titleId,
-  ingredients,
-  onIngredientClick
-}, ref) => {
+const IngredientsType = React.forwardRef(
+  (
+    {
+      title,
+      titleId,
+      ingredients,
+      // onIngredientClick
+    },
+    ref
+  ) => {
+    // Посчитаем сколько каждого элемента используется в готовом бургере
+    //Получим список элементов в конструкторе
+    const burgerConstructor = useSelector((state) => state.burgerConstructor);
+    const componentsCount = useMemo(() => {
+      const { bun, ingredients } = burgerConstructor;
+      const counters = {};
+      ingredients.forEach((ingredient) => {
+        if (!counters[ingredient._id]) counters[ingredient._id] = 0;
+        counters[ingredient._id] = counters[ingredient._id] + 1;
+      });
+      if (bun) counters[bun._id] = 2;
+      return counters;
+    }, [burgerConstructor]);
 
- // Посчитаем сколько каждого элемента используется в готовом бургере
-//Получим список элементов в конструкторе
-const burgerConstructor = useSelector(state => state.burgerConstructor)
-const componentsCount = useMemo(() => {
-  const { bun, ingredients } = burgerConstructor;
-  const counters = {};
-  ingredients.forEach((ingredient) => {
-    if (!counters[ingredient._id]) counters[ingredient._id] = 0;
-    counters[ingredient._id] = counters[ingredient._id] + 1;
-  });
-  if (bun) counters[bun._id] = 2;
-  return counters
-}, [burgerConstructor]);
-
-  return (
-    <>
-      <h2 className="text text_type_main-medium mt-10" id={titleId}>
-        {title}
-      </h2>
-      <div className={styles.type} ref={ref}>
-        {ingredients.map((ingredient) => {
-          return (
-            <Ingredient
-              ingredientData={ingredient}
-              key={ingredient._id}
-              count={componentsCount[ingredient._id]}
-              onClick={onIngredientClick}
-            />
-          );
-        })}
-      </div>
-    </>
-  );
-});
+    return (
+      <>
+        <h2 className="text text_type_main-medium mt-10" id={titleId}>
+          {title}
+        </h2>
+        <div className={styles.type} ref={ref}>
+          {ingredients.map((ingredient) => {
+            return (
+              <Ingredient
+                ingredientData={ingredient}
+                key={ingredient._id}
+                count={componentsCount[ingredient._id]}
+                // onClick={onIngredientClick}
+              />
+            );
+          })}
+        </div>
+      </>
+    );
+  }
+);
 
 IngredientsType.propTypes = {
   title: PropTypes.string.isRequired,
   titleId: PropTypes.string.isRequired,
   ingredients: PropTypes.arrayOf(IngredientPropTypes.isRequired).isRequired,
-  onIngredientClick: PropTypes.func.isRequired,
 };
 export default IngredientsType;
