36b5477b6beedc55d5889f6768caea863fb72b49
Ivan Vakulenko <ivan-vakulenko@yandex.ru>
2022-07-18 23:17:52 +0300

    Refactor closeModal without Dispatch

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,7 +1,7 @@
 import { Switch, Route } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
-import { useEffect } from "react";
-import { useLocation } from "react-router-dom";
+import { useEffect, useCallback } from "react";
+import { useLocation, useHistory } from "react-router-dom";
 import {
   LoginPage,
   HomePage,
@@ -18,22 +18,20 @@ import IngredientInfo from "../ingredient-info/ingredient-info";
 import { getItems } from "../../services/actions/ingredient";
 import { checkAuth } from "../../services/actions/user";
 import { ProtectedRoute } from "../protected-route/protected-route";
-import { closeIngredientModal } from "../../services/actions/ingredient-detail";
+//import { closeIngredientModal } from "../../services/actions/ingredient-detail";
 
 function App() {
   const dispatch = useDispatch();
   const { isAuthChecked } = useSelector((store) => store.user);
-  const ingredientInModal = useSelector((state) => state.ingredientDetail.info);
-  //console.log('isAuthChecked', isAuthChecked);
+
 
   const location = useLocation();
   //Опциональная цепочка ?. останавливает вычисление
   // и возвращает undefined, если значение перед ?. равно undefined или null.
   const background = location.state?.background;
   //let background = location.state && location.state.background;
-
-  // const accessToken = getCookie('accessToken');
-  // console.log(`Bearer ${accessToken}`);
+  const history = useHistory();
+  
   // При загрузке страницы запросим все ингредиенты с сервера
   useEffect(() => {
     dispatch(getItems());
@@ -41,9 +39,20 @@ function App() {
     dispatch(checkAuth());
   }, [dispatch]);
 
-  const closeModal = () => {
-    dispatch(closeIngredientModal());
-  };
+  //Если открыть попап ингредиента и закрыть его, то в адресной строке так и остаётся id ингредиента.
+  // А он должен пропадать. Для этого нужно делать переход назад по истории браузера.
+  // Dispatch не подходит.
+
+  // const closeModal = () => {
+  //   dispatch(closeIngredientModal());
+  // };
+  
+  const closeModal = useCallback(
+    (path) => {
+      history.push(path);
+      },
+    [history]
+  );
 
   // console.log("isAuthChecked", isAuthChecked);
   // console.log("background", background);
@@ -89,9 +98,9 @@ function App() {
             <Route
               path="/ingredients/:id"
               exact={true}
-              children={<IngredientInfo />}
-            />
-            {/* Страница профиля*/}
+              children={<IngredientInfo/>}
+            />       
+           {/* Страница профиля*/}
             <ProtectedRoute path="/profile" exact={true}>
               <ProfilePage />
             </ProtectedRoute>
@@ -102,11 +111,11 @@ function App() {
           </Switch>
 
           {/* Попап с модальным окном */}
-          {background && ingredientInModal && (
+          {background &&  (
             <Route
               path="/ingredients/:id"
               children={
-                <Modal onClose={closeModal}>
+                <Modal onClose={() => {closeModal('/')}}>
                   <IngredientInfo />
                 </Modal>
               }
