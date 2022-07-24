aa2d52f817bf51705f734462aafba75817817452
Ivan Vakulenko <ivan-vakulenko@yandex.ru>
2022-07-24 11:29:34 +0300

    edit

--- src/components/ingredient-info/ingredient-info.jsx
+++ src/components/ingredient-info/ingredient-info.jsx
@@ -5,10 +5,10 @@ import styles from "./ingredient-info.module.css";
 const IngredientInfo = () => {
   const { id } = useParams();
   //Вытяним из хранилища ингредиентов
-  const ingredients = useSelector((state) => state.ingredients.items);
-  // console.log ("ingredients", ingredients)
+  const ingredients = useSelector((store) => store.ingredients.items);
+  //console.log ("ingredients", ingredients)
   //И найдем неодходимый компонент для отображения
-  const ingredient = ingredients.find((ingredient) => ingredient._id === id);
+   const ingredient = ingredients.find((ingredient) => ingredient._id === id);
   // console.log ("ingredient", ingredient);
 
   return (
